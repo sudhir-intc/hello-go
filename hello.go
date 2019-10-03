31131af9aa411641321cb6e6f3a9fe46764fa1e0
Sudhir Pola <sudhir.pola@intel.com>
2019-10-03 13:50:37 +0530

    Added the time usage

--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 import (
 	"fmt"
 	"github.com/sudhir-intc/hello/string"
+	"time"
 )
 
 func  main(){
  fmt.Println("Hello, new world!")	
  fmt.Println(string.Reverse("Hello, new world!"))
+ time.Sleep(5000)
+ fmt.Println("After sleep of 5 seconds")
 }
