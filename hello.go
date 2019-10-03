a746ff72ec183de19a07491b9dc76f78a747b837
Sudhir Pola <sudhir.pola@intel.com>
2019-10-03 09:21:07 +0530

    Added sring as well as reversed value

--- hello.go
+++ hello.go
@@ -1,9 +1,10 @@
 package main
 import (
 	"fmt"
-	"github.com/sudhirpola/string"
+	"github.com/sudhir-intc/hello/string"
 )
 
 func  main(){
+ fmt.Println("Hello, new world!")	
  fmt.Println(string.Reverse("Hello, new world!"))
 }
